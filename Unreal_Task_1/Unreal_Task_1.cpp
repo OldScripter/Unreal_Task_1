5e0ad39a4a9adc93038df7a2281c040966b11030
OldScripter <Oldscripter@gmail.com>
2021-11-24 01:00:59 +0300

    Swap and print fucntion example

---
+++ Unreal_Task_1/Unreal_Task_1.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+/**
+* Prints the string in console.
+* @param [in] line - string to be printed out
+*
+*/
+void printLine(std::string line)
+{
+    std::cout << line << "\n";
+}
+
+/**
+* Swaps int values a and b.
+* @param [in/out] a - int value
+* @param [in/out] b - int value
+*/
+void swap(int& a, int& b)
+{
+    int temp{ a };
+    a = b;
+    b = temp;
+}
+
+int main()
+{
+    printLine("Let's try to swap elements:\n");
+    int a{ 3 };
+    int b{ 4 };
+
+    std::cout << "Initial values:\n";
+    std::cout << "\t - a = " << a << "\n";
+    std::cout << "\t - b = " << b << "\n";
+
+    std::cout << "SWAP!\n";
+    swap(a, b);
+    std::cout << "\t - a = " << a << "\n";
+    std::cout << "\t - b = " << b << "\n";
+    
+    return 0;
+}
